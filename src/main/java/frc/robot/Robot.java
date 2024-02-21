444c4fd939e9bc990ef22b45931d7752c93caf95
royalrobotics2522 <RoyalRobotics@outlook.com>
2024-02-20 18:56:50 -0800

    adding shufflleboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,6 +16,9 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+
+    OurShuffleboard shuffleboard = new OurShuffleboard(this);
+
   }
 
   @Override
